8ad06eb331e8be508009eb1f04df08c40a666ce1
TSCTiago <tiagodasilvacar@gmail.com>
2024-01-31 00:23:08 -0300

    change form color

--- src/components/input/style.ts
+++ src/components/input/style.ts
@@ -21,7 +21,8 @@ export const InputLabel = styled.label<labelProps>`
   transform: ${(props) => (props.filled === 'false' ? 'translate(0, 12px) scale(0.8)' : 'translate(0, 23px) scale(1)')};
   transform-origin: top left;
   transition: 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
-  color: #ffffff;
+  /* color: #ffffff; */
+  color: black;
   font-weight: 700;
   font-size: 16px;
   line-height: 1;
@@ -32,18 +33,18 @@ const baseInputStyles = `
   height: 64px;
   transform-origin: top left;
   border-radius: 4px;
-  border: none;
+  border: 2px solid #2199bf;
   padding: 24px 16px 4px 16px;
   font-size: 16px;
   line-height: 1;
   outline: none;
   box-shadow: none;
   transition: 200ms cubic-bezier(0, 0, 0.2, 1) 0ms;
-  background: #2199bf;
-  color: #FFFFFF;
+  // background: #10607A;
+  color: #000;
 
   &:focus {
-    box-shadow: 0 0 0 2px #79b1ff;
+    box-shadow: 0 0 0 2px #2199bf;
   }
 `;
 
@@ -53,6 +54,7 @@ export const InputElement = styled.input`
 `;
 
 export const TextArea = styled.textarea`
+  max-width: 100%;
   ${baseInputStyles}
   height: 128px;
   padding: 34px 16px 4px 16px;
